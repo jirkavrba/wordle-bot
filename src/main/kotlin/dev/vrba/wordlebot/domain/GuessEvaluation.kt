5c4538c0420702156564a581376548926f8ccf77
Jiří Vrba <jiri@vrba.dev>
2022-05-28 11:07:52 +0200

    Add methods for creating the letter mask and matching it against evaluations

--- src/main/kotlin/dev/vrba/wordlebot/domain/GuessEvaluation.kt
+++ src/main/kotlin/dev/vrba/wordlebot/domain/GuessEvaluation.kt
@@ -1,11 +1,46 @@
 package dev.vrba.wordlebot.domain
 
+import kotlin.math.abs
+
 data class GuessEvaluation(
     val guess: String,
     val evaluation: List<LetterEvaluation>
 ) {
     override fun toString(): String {
         return evaluation.joinToString("") { it.emoji }
+    }
+
+    /**
+     * Provides a letter mask that can be quickly applied to check if the guess evaluation matches the evaluation or not,
+     * for example, the mask for "GUESS", scored as 🟨🟥🟥🟥🟩 will result in a mask containing:
+     * All letters except the letter 'G' for the first position
+     * All letters for positions from 2 up to 4
+     * Only the letter "S" for position 5
+     */
+    fun letterMask(): List<Set<Char>> {
+        val alphabet = ('a' .. 'z').toSet()
+        val zipped = guess.toList().zip(evaluation)
+
+        // Letters that cannot appear inside the word
+        val absent = zipped
+            .filter { it.second == LetterEvaluation.Absent }
+            .map { it.first }
+            .toSet()
+
+        return zipped.map {  (letter, evaluation) ->
+            when (evaluation) {
+                LetterEvaluation.Correct -> setOf(letter)
+                LetterEvaluation.Present -> alphabet - absent - letter
+                LetterEvaluation.Absent -> alphabet - absent
+            }
+        }
+    }
+}
+
+fun matchesEvaluations(word: String, evaluations: List<GuessEvaluation>): Boolean {
+    return evaluations.all {
+        // For each evaluation, check, that the letter mask can be matched with this word
+        return it.letterMask().zip(word.toList()).all { (letters, letter) -> letter in letters }
     }
 }
 
